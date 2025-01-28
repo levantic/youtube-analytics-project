0bdb75e2e45076ce35259ab8a498206e0ad2dc84
levantic <ilija.yershov@gmail.com>
2025-01-28 08:15:18 +0300

    added youtube_channel 1 homework

--- src/channel.py
+++ src/channel.py
@@ -6,7 +6,7 @@ import googleapiclient.discovery
 import googleapiclient.errors
 import json
 
-from helper.youtube_api_manual import printj
+#from helper.youtube_api_manual import printj
 from googleapiclient.discovery import build
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
@@ -19,16 +19,20 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
 
     def print_info(self) -> None:
         #Выводит в консоль информацию о канале.
-        # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        #channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         printj(channel)
